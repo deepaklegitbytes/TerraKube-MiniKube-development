41a901be33a127dc66bb6cd90bbcfddfc3ff445b
Deeoak Kumar <deeoakkumar@Deeoaks-MacBook-Air.local>
2025-07-22 17:14:58 +0530

    fix auth config

--- ui/src/config/authUser.ts
+++ ui/src/config/authUser.ts
@@ -2,7 +2,7 @@ import { User } from "oidc-client-ts";
 
 export function getUserFromStorage(): User | null {
   const oidcStorage = localStorage.getItem(
-    `oidc.user:https://terrakube-dex.platform.local/dex:terrakube-app`
+    `oidc.user:${window._env_.REACT_APP_AUTHORITY}:${window._env_.REACT_APP_CLIENT_ID}`
   );
   console.log("OIDC storage item:", oidcStorage);
   if (!oidcStorage) {
